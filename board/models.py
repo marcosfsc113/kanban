4db47d3bd19660d31a401f0ce15c084b521efc94
marcosfsc113 <jigglippaf@gmail.com>
2025-06-27 20:51:51 -0300

    Definição das tabelas do BD usando o ORM do Django

--- board/models.py
+++ board/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Bucket(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome #Como o card vai aparecer?
+
+class Card(models.Model):
+    nome_paciente = models.CharField(max_length=100)
+    data_admissao = models.DateField()
+    estado_civil = models.CharField(max_length=30)
+    idade = models.IntegerField()
+    sexo = models.CharField(max_length=10)
+    bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE, related_name='cards')
+
+    def __str__(self):
+        return f"{self.nome_paciente} ({self.bucket.nome})" #Exibicao do nome juntamente com a etapa do paciente
