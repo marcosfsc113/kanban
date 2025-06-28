a885d30e8505e3bb95aa4743c0d43ddc5cded8e3
marcosfsc113 <jigglippaf@gmail.com>
2025-06-28 15:10:28 -0300

    Importando a view home

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from board.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
