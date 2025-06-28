1f6f2af0ce2d292e93c83cfa82107df5a02596c1
marcosfsc113 <jigglippaf@gmail.com>
2025-06-27 23:27:04 -0300

    Corrigindo caminho dos arquivos serializer e views

--- board/views.py
+++ board/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets #importar viewset para nao ser necessario fazer o CRUD na mao
+from .models import Bucket, Card
+from .serializers import BucketSerializer, CardSerializer
 
-# Create your views here.
+class BucketViewSet(viewsets.ModelViewSet):
+    #ModelCiewSet relaciona os modelos com as rotas
+    queryset = Bucket.objects.all()
+    serializer_class = BucketSerializer
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
