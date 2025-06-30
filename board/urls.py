3d1f7d50ab86fedd8b9700b4d580fe1d31db12f0
marcosfsc113 <jigglippaf@gmail.com>
2025-06-29 22:25:05 -0300

    Adicionando rotas para atualizar status e detalhes do card

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include 
 from rest_framework.routers import DefaultRouter
-from .views import BucketViewSet, CardViewSet, home_view
-
+from .views import BucketViewSet, CardViewSet, CardUpdateStatusView, CardDetailView, home_view
 router = DefaultRouter() #Gera rotas automaticamente
 router.register(r'buckets', BucketViewSet)
 router.register(r'cards', CardViewSet)
@@ -9,4 +8,6 @@ router.register(r'cards', CardViewSet)
 urlpatterns = [
     path('', home_view, name='home'),
     path('', include(router.urls)), #Junta as rotas geradas dentro do app
+    path('cards/<int:pk>/update_status/', CardUpdateStatusView.as_view(), name='card-update-status'),
+    path('cards/<int:pk>/', CardDetailView.as_view(), name='card-detail'),
 ]
